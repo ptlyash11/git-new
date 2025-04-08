961c8cf0d621197ba9e3cc6ece358d12b090d7b0
Yash patel <ypatel@localhost.localdomain>
2025-04-08 16:59:51 -0600

    update 2 time

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:0001');
+  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:00054321x');
 });
 
 const server = app.listen(PORT, HOST);
